85f93eb1b4bbdba01a7d2f1aa4ef3269c2c7f143
hoan3232 <76396996+hoan3232@users.noreply.github.com>
2022-07-22 16:41:38 +0700

    Server side functions

--- src/main/java/group_02/server/db/DB.java
+++ src/main/java/group_02/server/db/DB.java
@@ -1,15 +1,14 @@
 package group_02.server.db;
+
+import group_02.server.models.Enote;
+
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DB {
-    public static Connection connectDB(String DB_URL ) {
-        Driver myDriver = new com.microsoft.sqlserver.jdbc.SQLServerDriver();
-        try {
-            DriverManager.registerDriver(myDriver);
-        } catch (SQLException e) {
-        }
+    public static Connection conn = null;
 
-        Connection conn = null;
+    public static Connection connectDB(String DB_URL) {
         try {
             conn = DriverManager.getConnection(DB_URL);
         } catch (SQLException e) {
@@ -17,13 +16,107 @@ public class DB {
         if (conn != null) {
             System.out.println("Connected");
         }
-
         return conn;
-
     }
-    public static void main(String args[])  {
-        String url = "jdbc:sqlserver://localhost:1433;databaseName=COVIDMANAGEMENT;user=sa;password=1;trustServerCertificate=true";
-        Connection conn = connectDB(url);
 
+    /**
+     * @param username
+     * @param password
+     * @return "success" || "wrong password" || "username does not exist" || "error"
+     */
+    public static String signIn(String username, String password) {
+        String result = "error";
+        try {
+            CallableStatement cstmt = conn.prepareCall("{call dbo.usp_signin(?,?,?)}");
+            cstmt.setString(1, username);
+            cstmt.setString(2, password);
+            cstmt.registerOutParameter(3, Types.VARCHAR);
+            cstmt.execute();
+
+            result = cstmt.getNString(3);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * @param username
+     * @param password
+     * @return "success" || "username already taken" || "error"
+     */
+    public static String signUp(String username, String password) {
+        String result = "error";
+        try {
+            CallableStatement cstmt = conn.prepareCall("{call dbo.usp_signup(?,?,?)}");
+            cstmt.setString(1, username);
+            cstmt.setString(2, password);
+            cstmt.registerOutParameter(3, Types.VARCHAR);
+            cstmt.execute();
+
+            result = cstmt.getNString(3);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * @param username
+     * @param filePath
+     * @param fileType
+     * @return "success" || "error"
+     */
+    public static String saveEnote(Enote enote) {
+        String result = "error";
+        try {
+            PreparedStatement ps = conn.prepareCall("insert into enote(username,files_path,files_type) values(?,?,?)");
+            ps.setString(1, enote.getUsername());
+            ps.setString(2, enote.getFilePath());
+            ps.setString(3, enote.getFilePath());
+            ps.execute();
+
+            result = "success";
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static ArrayList<Enote> getEnoteList(String username) {
+        ArrayList <Enote> noteList = new ArrayList<>();
+        try (CallableStatement cstmt = conn.prepareCall("select * from Enote where username = ?");) {
+            cstmt.setString(1, username);
+
+            boolean result = cstmt.execute();
+            if (result) {
+                ResultSet rs = cstmt.getResultSet();
+                while (rs.next()) {
+                    noteList.add(
+                            new Enote(
+                                    rs.getInt("id_note"),
+                                    rs.getString("username"),
+                                    rs.getString("files_path"),
+                                    rs.getString("files_type")
+                            )
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return noteList;
+    }
+
+
+    public static void main(String args[]) {
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=Enote;user=sa;password=1;trustServerCertificate=true";
+        Connection conn = connectDB(url);
+        //System.out.println(signIn("hoan3232","123"));
+
+        //System.out.println(saveEnote(new Enote("hoan3232", "aaaaa", "aaa")));
+        System.out.println(getEnoteList("hoan3232"));
     }
 }
